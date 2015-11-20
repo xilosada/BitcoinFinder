6db2afd0f540f065a2ca6b5d8a2ccda1e3668bf9
Xabier I. Losada <xi.losada@aditium.com>
2015-11-20 10:49:44 +0100

    Initial Commit

---
+++ BitcoinFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitcoinFinder
+//
+//  Created by X.I. Losada on 20/11/15.
+//  Copyright © 2015 xiLosada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
